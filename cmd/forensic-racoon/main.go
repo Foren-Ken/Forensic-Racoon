596e9a01083ae8ddaf8493eff04d208baf35c272
Ken <143446633+Foren-Ken@users.noreply.github.com>
2025-04-01 16:05:31 -0400

    Updated main.go

--- cmd/forensic-racoon/main.go
+++ cmd/forensic-racoon/main.go
@@ -3,18 +3,31 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
-	var typeFlag = flag.String("t", "directory", "Specify either \"Directory\" or \"File\".")
+	typeFlag := flag.String("t", "directory", "Specify either \"Directory\" or \"File\".")
 	//Used to determine if the entire directory or just a singular file is being tested.
 
-	var outputFlag = flag.String("o", ".", "Specify output location.")
+	outputFlag := flag.String("o", ".", "Specify output location.")
 	// Stores location of output file
 
 	flag.Parse()
+	// Provides the parsing.
 
-	fmt.Println(*typeFlag)
+	userFilePath := flag.Args()
+
+	if len(userFilePath) < 1 {
+		fmt.Println("Error: Filepath is required")
+		fmt.Println("The following syntax is expected: .\\ PATH\\TO\\FILE")
+		os.Exit(1)
+	}
+	doesFileExist, err := os.Stat(userFilePath[0])
+
+	fmt.Println(doesFileExist)
+	fmt.Println(err)
 	fmt.Println(*outputFlag)
+	fmt.Println(*typeFlag)
 
 }
